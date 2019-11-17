46cc5c4e6610f0f47998b77bc2e1e2ef469c6d7f
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-17 20:34:01 +0200

    fixed post error

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var multer = require("multer");
+// Handle File Uploads
+var upload = multer({ dest: "./uploads" });
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -8,6 +11,13 @@ router.get('/', function (req, res, next) {
 
 router.get('/register', function (req, res, next) {
   res.render('register', { title: 'Register' });
+});
+
+router.post('/register', upload.single("profileimage"), function (req, res, next) {
+  // Getting values
+  var name = req.body.name;
+  var username = req.body.username;
+  console.log(name + username);
 });
 
 module.exports = router;
