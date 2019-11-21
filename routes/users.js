289471ca3f6254aad31612a9eaea94970949c77e
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-21 23:12:19 +0200

    passport strategy addded

--- routes/users.js
+++ routes/users.js
@@ -50,7 +50,22 @@ router.get('/login', function (req, res, next) {
 });
 
 // Authenticate login
-
+router.post('/login', function (req, res, next) {
+  passport.use(new LocalStrategy(
+    function (username, password, done) {
+      User.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      });
+    }
+  ));
+});
 
 
 
