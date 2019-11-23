9545327870d2c2a8dbb6cab7519d6d9f3a6f761c
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-23 19:20:21 +0200

    file addded

---
+++ config/passport.js
@@ -0,0 +1,32 @@
+
+
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        // Get form data
+        var username = 'username';
+        var password = 'password';
+
+        User.findOne({ username: username }, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }
+));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
