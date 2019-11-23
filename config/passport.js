854b0c40dc54fb70bdef4fd90afddadb761f6816
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-23 20:14:15 +0200

    login username error fixed

--- config/passport.js
+++ config/passport.js
@@ -5,29 +5,26 @@ const User = require("../models/users_db");
 
 
 module.exports = function (passport) {
-    passport.use(new LocalStrategy(
+    passport.use(new LocalStrategy({ username: 'username' },
         function (username, password, done) {
-            // Get form data
-            var username = 'username';
-            var password = 'password';
 
-            User.findOne({ username: username }, function (err, user) {
+            User.findOne({ username: username }, function (err, users) {
                 // Error Handler
                 if (err) {
                     return done(err);
                 }
                 // If username does not exist
-                if (!user) {
+                if (!users) {
                     return done(null, false, { message: 'Incorrect username.' });
                 }
                 // If password does not exist
                 // password - is what the user types into the form
                 // user.password - is the user's password that is stored in the database
-                bcryt.compare(password, user.passord, (err, isMatch) => {
+                bcryt.compare(password, users.password, (err, isMatch) => {
                     if (err) throw err;
 
                     if (isMatch) {
-                        return done(null, user);
+                        return done(null, users);
                     } else {
                         return done(null, false, { message: 'Incorrect password.' });
                     }
@@ -37,13 +34,13 @@ module.exports = function (passport) {
         }
     ));
 
-    passport.serializeUser(function (user, done) {
-        done(null, user.id);
+    passport.serializeUser(function (users, done) {
+        done(null, users.id);
     });
 
     passport.deserializeUser(function (id, done) {
-        User.findById(id, function (err, user) {
-            done(err, user);
+        User.findById(id, function (err, users) {
+            done(err, users);
         });
     });
 }
