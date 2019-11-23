b954bd5f6d1feedd32ed4dbee0901aee0301058e
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-24 00:24:54 +0200

    code clean up

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ const { ensureAuthenticated } = require('../config/auth');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  console.log(req.isAuthenticated());
   res.render('index', { title: 'Home', isLoggedIn: req.isAuthenticated() });
 });
 
+
+
 router.get('/dashboard', ensureAuthenticated, function (req, res, next) {
-  console.log(req.isAuthenticated());
   res.render('dashboard', { title: 'Dashboard', username: req.user.name });
 });
 module.exports = router;
