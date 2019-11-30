0e287eed77b77ada6eeb2a812324483b4ca3ead7
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-30 17:03:49 +0200

    subdocs schema created for anime list

--- models/users_db.js
+++ models/users_db.js
@@ -3,6 +3,12 @@ var mongoose = require("mongoose");
 //Define a schema
 var Schema = mongoose.Schema;
 
+var animeSchema = new Schema({
+    _id: Schema.Types.ObjectId,
+    title: String,
+    source: String
+})
+
 // Creating a schema properties
 var users = new Schema({
     _id: Schema.Types.ObjectId,
@@ -10,8 +16,12 @@ var users = new Schema({
     username: { type: String, required: true },
     email: { type: String, lowercase: true, required: true, },
     password: { type: String, minlength: 6 },
-    profileImage: String
+    profileImage: String,
+    anime: [animeSchema]
 });
 
 
-module.exports = mongoose.model('User', users)
+
+
+module.exports = mongoose.model('User', users);
+module.exports = mongoose.model('Anime', animeSchema);
